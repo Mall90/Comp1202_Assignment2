6d4d3f3bb0051fc5c95b3fb1378ca73d5e4b887b
Mustafa <mustafa_allsaid@yahoo.ca>
2021-08-14 17:18:08 -0400

    first commit

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.IO;
+
+
+namespace TESTTESTSTSTST
+{
+    class Program
+    {
+        public static int StudentSize = 15;
+        public static Student[] students1 = new Student[StudentSize];
+
+        static void Main(string[] args)
+        {
+            string menuChoice;
+
+            do
+            {
+                ShowMenu();
+                Console.Write(">>> ");
+                menuChoice = Console.ReadLine();
+
+                switch (menuChoice)
+                {
+                    case "1":
+                        AddStudent();
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+
+                    case "2":
+                        ListStudents();
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+
+                    case "3":
+                        FindStudent();
+                        Console.WriteLine("Press any key to continue...");
+                        Console.ReadKey();
+                        break;
+
+                }
+            } while (menuChoice != "4");
+
+        }
+
+        public static void ShowMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("***************************************************");
+            Console.WriteLine("**          STUDENT INFORMATION          **");
+            Console.WriteLine("***************************************************");
+            Console.WriteLine();
+            Console.WriteLine("Enter one of the options below...");
+            Console.WriteLine();
+            Console.WriteLine("1.\tAdd Student");
+            Console.WriteLine("2.\tView Student List");
+            Console.WriteLine("3.\tFind A Specific Student");
+            Console.WriteLine("4.\tExit");
+        }
+        public static void AddStudent()
+        {
+            try
+            {
+                for (int i = 0; i < students1.Length; i++)
+                {
+                    if (students1[i] == null)
+                    {
+                        Console.Clear();
+                        Console.WriteLine(">>>>>>>> Add Student <<<<<<<<");
+
+                        Console.Write($">>> Student ID: ");
+                        int stuID = int.Parse(Console.ReadLine());
+
+                        Console.Write($">>> Student's first name: ");
+                        string fName = Console.ReadLine();
+
+                        Console.Write($">>> Student's last name: ");
+                        string lName = Console.ReadLine();
+
+                        Console.Write($">>> Student's Major: ");
+                        string stuMajor = Console.ReadLine();
+
+                        Console.Write($">>> Student's Phone Number: ");
+                        string phone = Console.ReadLine();
+
+                        Console.Write($">>> Student's GPA: ");
+                        double stuGPA = double.Parse(Console.ReadLine());
+
+                        Console.Write($">>> Student's Day of Birth: ");
+                        int dob = int.Parse(Console.ReadLine());
+
+                        Console.Write($">>> Student's Month of Birth: ");
+                        int mob = int.Parse(Console.ReadLine());
+
+                        Console.Write($">>> Student's Year of Birth: ");
+                        int yob = int.Parse(Console.ReadLine());
+
+                        Student student = new Student(
+                            studentID: stuID,
+                            firstName: fName,
+                            lastName: lName,
+                            major: stuMajor,
+                            phone: phone,
+                            gpa: stuGPA,
+                            dayOfBirth: dob,
+                            monthOfBirth: mob,
+                            yearOfBirth: yob
+                            );
+
+                        Console.WriteLine();
+                        Console.WriteLine(student.ToInfo());
+
+                        students1[i] = student;
+                        Console.WriteLine(">>> Student Information saved successfully!");
+                        return;
+                    }
+                }
+
+                Console.WriteLine("Sorry... Student List is already full!");
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+                Console.WriteLine(">>> Student information wasn't saved! Please try agian!");
+            }
+        }
+
+        public static void ListStudents()
+        {
+            Console.Clear();
+            Console.WriteLine(">>>>>>>> View Student List <<<<<<<<");
+            Console.WriteLine($"Student Name \t\t\t GPA");
+            Console.WriteLine();
+
+            int nullCount = 0;
+
+            for (int i = 0; i < students1.Length; i++)
+            {
+                if (students1[i] == null)
+                {
+                    nullCount += 1;
+                }
+                else
+                {
+                    Console.WriteLine($"{i + 1}. {students1[i].GetFullName()} \t\t {students1[i].GetGPA()}");
+                }
+            }
+
+            Console.WriteLine();
+
+            if (nullCount == students1.Length)
+            {
+                InformEmptyBook();
+            }
+        }
+
+        public static void FindStudent()
+        {
+            Console.Clear();
+            Console.WriteLine(">>>>>>>> Find A Student <<<<<<<<");
+            Console.WriteLine();
+
+            string[] studentToFind = GrabStudent(action: "view");
+            int nullCount = 0;
+            int nameNotMatch = 0;
+
+            for (int i = 0; i < students1.Length; i++)
+            {
+                if (students1[i] == null)
+                {
+                    nullCount += 1;
+                    nameNotMatch += 1;
+                }
+                else
+                {
+                    if (students1[i].FirstName.ToLower() == studentToFind[0] &&
+                        students1[i].LastName.ToLower() == studentToFind[1])
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine(">>>>>>>> Student Found <<<<<<<<\n");
+                        Console.WriteLine(students1[i].ToInfo());
+                        break;
+                    }
+                    else
+                    {
+                        nameNotMatch += 1;
+                    }
+                }
+            }
+        }
+
+        public static string[] GrabStudent(string action)
+        {
+            var fullName = new string[2];
+
+            Console.WriteLine($"To {action} the student, please enter:");
+            Console.Write($"> First name: ");
+            string fName = Console.ReadLine().ToLower();
+            fullName[0] = fName;
+
+            Console.Write($"> Last name: ");
+            string lName = Console.ReadLine().ToLower();
+            fullName[1] = lName;
+
+            return fullName;
+        }
+
+        public static void InformEmptyBook()
+        {
+            Console.WriteLine();
+            Console.WriteLine(">>>>>>>> The student list is empty <<<<<<<<");
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ ConsoleApp2/Student.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TESTTESTSTSTST
+{
+    class Student
+    {
+        // fields
+        private int idgenerator = 10000;
+        private int studentid;
+        private string firstName;
+        private string lastName;
+        private string major;
+        private string phone;
+        private double gpa;
+        private int dayOfBirth;
+        private int monthOfBirth;
+        private int yearOfBirth;
+
+        // properties
+        public int IDGenerator { get => idgenerator; set => idgenerator = value; }
+        public int StudentID { get => studentid; set => studentid = value; }
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Major { get => major; set => major = value; }
+        public string Phone { get => phone; set => phone = value; }
+        public double GPA { get => gpa; set => gpa = value; }
+        public int DayOfBirth { get => dayOfBirth; set => dayOfBirth = value; }
+        public int MonthOfBirth { get => monthOfBirth; set => monthOfBirth = value; }
+        public int YearOfBirth { get => yearOfBirth; set => yearOfBirth = value; }
+
+
+        // constructors
+        public Student(int studentID, string firstName, string lastName, string major, string phone, double gpa, int dayOfBirth, int monthOfBirth, int yearOfBirth)
+        {
+            this.StudentID = studentID;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Major = major;
+            this.Phone = phone;
+            this.GPA = gpa;
+            this.DayOfBirth = dayOfBirth;
+            this.MonthOfBirth = monthOfBirth;
+            this.YearOfBirth = yearOfBirth;
+        }
+
+        // properties
+
+        public int GetIDGenerator()
+        {
+            return idgenerator;
+        }
+
+
+        public int GetStudentID()
+        {
+            return studentid;
+        }
+
+        public string GetFirstName()
+        {
+            return firstName;
+        }
+
+        public string GetLastName()
+        {
+            return lastName;
+        }
+
+        public string GetFullName()
+        {
+            // Return first and last name of contact as a string
+            return $"{firstName} {lastName}";
+        }
+
+        public string GetMajor()
+        {
+            return major;
+        }
+
+        public string GetPhone()
+        {
+            return phone;
+        }
+
+
+        public double GetGPA()
+        {
+            return gpa;
+        }
+
+        public string GetDateOfBirth()
+        {
+            return $"{dayOfBirth}-{monthOfBirth}-{yearOfBirth}";
+        }
+
+        // Displays Student information
+        public string ToInfo()
+        {
+            return "*********** Student Information ***********" +
+                    "\nIDGenerator: \t\t" + $"{idgenerator}" +
+                    "\nStudentID: \t\t" + $"{studentid}" +
+                    "\nFirst Name: \t\t" + $"{firstName}" +
+                    "\nLast Name: \t\t" + $"{lastName}" +
+                    "\nMajor \t\t\t" + $"{major}" +
+                    "\nPhone: \t\t\t" + $"{phone}" +
+                    "\nGPA: \t\t\t" + $"{gpa}" +
+                    "\nDate of Birth: \t\t" + $"{dayOfBirth}-{monthOfBirth}-{yearOfBirth}" +
+                    "\n";
+        }
+    }
+}
+
